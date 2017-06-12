b87419cbcfa7df1a9c6348df004cb2ed6704c859
xingwanwan <986402004@qq.com>
2017-06-12 20:57:32 +0800

    first4

--- config/configrouter.js
+++ config/configrouter.js
@@ -1,23 +1,13 @@
-index=require("../router/index.js")
-detil=require("../router/login.js")
-logined=require("../router/logined.js")
-deletes=require("../router/delete.js")
-add=require("../router/add.js")
+
 name=require("../router/name.js")
 xia=require("../router/xia.js")
  xianame=require("../router/xianame.js")
 tianjia=require("../router/tianjia.js")
 
-//repalce=require("../router/replce.js")
 module.exports=function(app){
-	app.get("/index",index)
 	app.get("/name",name)
-	app.get("/detil",detil)
 	 app.get("/xianame",xianame)
 	app.post("/tianjia",tianjia)
-	app.post("/logined",logined)
-	app.post("/delete",deletes)
-	app.post("/add",add)
 	app.post("/xia",xia)
 	//app.post("/repalce",repalce)
 }
